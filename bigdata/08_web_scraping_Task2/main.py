a39bdb13b60cfd22bcafdf71f9e52bf3341b8eb4
LeeSooo <dltn990930@naver.com>
2023-04-07 20:50:11 +0900

    feat : 빅데이터 08 (2023-04-07)

---
+++ bigdata/08_web_scraping_Task2/main.py
@@ -0,0 +1,13 @@
+# 문제2 : 가비아 컨테이너호스팅 페이지에서 Python 호스팅 정보 추출.
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get("https://webhosting.gabia.com/container/service")
+soup = BeautifulSoup(res.content, 'html.parser')
+
+python_tab = soup.find(id="_tab1-2")
+python_table = soup.find(class_="brick-plan-prod__table").find("tbody")
+
+for tag in python_tab.find_all(class_='brick-plan-prod__name python'):
+    print("종류 : " + tag.string)
+
