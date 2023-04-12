5416b841502685379f324c2870b9c1a8b4d0eb41
LeeSooo <dltn990930@naver.com>
2023-04-12 16:47:33 +0900

    feat : Cos Pro 1급 공부하기 (2023-04-12)

--- bigdata/10_web_scraping_Task4/main.py
+++ bigdata/10_web_scraping_Task4/main.py
@@ -2,20 +2,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-i = 0
-out = True
-
-while(out):
-    i+=1
-    res = requests.get("https://www.hollys.co.kr/store/korea/korStore2.do?pageNo="+str(i))
+no = 1
+while(True):
+    res = requests.get("https://www.hollys.co.kr/store/korea/korStore2.do?pageNo="+str(no))
     soup = BeautifulSoup(res.content, 'html.parser')
-    tbody = soup.select_one('tbody')
 
-    for store_tr in tbody.select('tr'):
-        if(store_tr.td.string=='등록된 지점이 없습니다.'):
-            out=False
-            break
-
-        for store_td in store_tr.select('td'):
-            print(store_td.string,end='|')
-        print('\n------------------------------')
+    for tr in soup.select_one('tbody').select('tr'):
+        for td in len(tr.select('td')):
+            print(tr.select('td')[td].text)
+    no += 1
