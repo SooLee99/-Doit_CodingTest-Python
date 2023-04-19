4baf68d2e43d4c6c1d3ff522f748c75bd37fae9e
LeeSooo <dltn990930@naver.com>
2023-04-19 21:21:21 +0900

    feat : 빅데이터 공부하기 (2023-04-19)

--- bigdata/01_pe1/myscoreboard.py
+++ bigdata/01_pe1/myscoreboard.py
@@ -5,26 +5,27 @@ def get_score_board(filename):
     file.close()
 
     # 학생들의 2차원 리스트 답변 목록
-    answers = []
+    students = []
 
     # 입력 데이터 전처리
     for line in lines:
-        answers.append([i.strip() for i in line.lower().split(',')])
+        students.append([i.strip() for i in line.lower().split(',')])
+       # students.append([i.strip() for i in line.lower().split(',')])
 
     # 문제의 정답만 따로 분리.
-    c_answers = answers.pop(0)
+    answers = students.pop(0)
 
     # 2차원 리스트에 나머지 학생 답변을 저장
     score_board = []
 
     # 루프에 인덱스 기반 사용 (학생 답변에 정답을 체크한다.)
-    for i in range(len(answers)):
+    for i in range(len(students)):
         # 학생의 리스트(세로) 생성.
         score_board.append([])
 
         # 답변의 정답을 체크하고, 리스트(가로) 작성.
-        for j in range(len(c_answers)):
-            if answers[i][j] == c_answers[j]:
+        for j in range(len(answers)):
+            if students[i][j] == answers[j]:
                 score_board[i].append(1)
             else:
                 score_board[i].append(0)
